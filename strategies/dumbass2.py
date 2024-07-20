7993c3c1b5a820c78ad9951507716807c0c85403
Lalit M <lalitm1004@gmail.com>
2024-07-20 22:41:49 +0530

    add more strats

--- strategies/dumbass2.py
+++ strategies/dumbass2.py
@@ -4,7 +4,7 @@ from utils.types import History, Move
 class Strategy:
 
     def __init__(self):
-        self.name = 'dumbass'
+        self.name = 'dumbass2'
 
     def begin(self):
         return Move.SPLIT
@@ -16,7 +16,7 @@ class Strategy:
             for i in range(3):
                 if history[-i-1].opponent == Move.STEAL:
                     y+=1
-            if y>=2:
+            if y>=1:
                 return Move.STEAL
             else:
                 return Move.SPLIT
