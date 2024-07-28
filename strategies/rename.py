0fed3d32b6d2eedc846181c32b71a82ad0d4c244
Lalit M <lalitm1004@gmail.com>
2024-07-28 15:46:54 +0530

    Prepare for public usage

---
+++ strategies/rename.py
@@ -0,0 +1,37 @@
+import random
+import math
+import numpy
+
+from utils.types import History, Move
+
+class Strategy:
+    def __init__(self) -> None:
+        self.name = ''
+        self.description = ''
+    
+    def begin(self) -> Move:
+        """
+            Logic for the first move of any showdown.
+            Is only run once on the initial run.
+
+            return
+                Move
+        """
+        
+        # Logic Here
+        pass
+
+    def turn(self, history: History) -> Move:
+        """
+            Logic for any move in the showdown after the first move.
+            Is run every time except on the initial run.
+
+            arguments
+                history: History
+  
+            return
+                Move
+        """
+        
+        # Logic Here
+        pass
